9b728959c549847c3bd91b5ab115da5985e1d87b
Tanay Soni <tanayvsoni@gmail.com>
2022-06-24 11:05:32 -0600

    rev:02

--- main.c
+++ main.c
@@ -4,11 +4,13 @@ int main()
 {
 
    int i, j;
-   int my_array[3][5] = {
-      {1,2,3,4,5},
-      {1,2,3,4,5},
-      {1,2,3,4,5},
-   };
+   int my_array[9][9] = {};
+
+   for(int i = 0; i < 9; i++){
+      for(int j = 0; j < 9; j++){
+         my_array[i][j] = 0;
+      }
+   }
 
    int row = sizeof(my_array)/sizeof(my_array[0]);
    int col = sizeof(my_array[0])/sizeof(my_array[0][0]); 
