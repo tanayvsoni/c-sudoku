e369d74432ee8671664512be319bbdb4d0a7b5e4
Tanay Soni <tanayvsoni@gmail.com>
2022-07-01 14:13:57 -0600

    sudoku

--- main.c
+++ main.c
@@ -3,6 +3,24 @@
 #include <string.h>
 #include <time.h>
 
+void display_screen(int **puzzle){
+
+   char divider[35] = "+---------+---------+---------+";
+
+   printf("%s\n", divider);
+   for (int i = 0; i < 9; i++) {
+
+      for (int j = 0; j < 9; j++) {
+
+         if(j == 0 || j % 3 == 0){printf("|");}
+         printf(" %d ", puzzle[i][j]);
+         if((j+1) == 9){printf("|");}
+      }
+      printf("\n");
+      if((i+1) % 3 == 0){printf("%s\n", divider);}
+   }
+}
+
 void rotate_arr(int *arr, int rotate_amount){
    int length_arr = 9;
 
@@ -20,40 +38,52 @@ void rotate_arr(int *arr, int rotate_amount){
 
 }
 
-void swap(int **puzzle){
+void swap_col(int **puzzle){
+   srand(time(NULL));
+   int swap[2] = {0};
+
+   // Swap Cols
+   for(int k = 0; k < 3; k++){
+      for(int swaps = 0; swaps < 3; swaps++){
+         while(swap[0] == swap[1]){
+            swap[0] = (rand() % 3) + k*3;
+            swap[1] = (rand() % 3) + k*3;
+         }
+
+         for(int i = 0; i < 9; i++){
+            int temp = puzzle[i][swap[0]];
+            puzzle[i][swap[0]] = puzzle[i][swap[1]];;
+            puzzle[i][swap[1]] = temp;
+         }
+         memset(swap,0,sizeof(swap)); 
+      }  
+   }
+}
+
+void swap_rows(int **puzzle){
    srand(time(NULL));
    int swap[2] = {0};
 
    // Swap Rows
    for(int k = 0; k < 3; k++){
+      for(int swaps = 0; swaps < 3; swaps++){
+         while(swap[0] == swap[1]){
+            swap[0] = (rand() % 3) + k*3;
+            swap[1] = (rand() % 3) + k*3;
+         }
 
-      while(swap[0] == swap[1]){
-         swap[0] = (rand() % 3) + k*3;
-         swap[1] = (rand() % 3) + k*3;
+         int *temp = puzzle[swap[0]];
+         puzzle[swap[0]] = puzzle[swap[1]];
+         puzzle[swap[1]] = temp;
+
+         memset(swap,0,sizeof(swap)); 
       }
-
-      int *temp = puzzle[swap[0]];
-      puzzle[swap[0]] = puzzle[swap[1]];
-      puzzle[swap[1]] = temp;
-
-      memset(swap,0,sizeof(swap)); 
    }
+}
 
-   // Swap Cols
-   for(int k = 0; k < 3; k++){
-
-      while(swap[0] == swap[1]){
-         swap[0] = (rand() % 3) + k*3;
-         swap[1] = (rand() % 3) + k*3;
-      }
-
-      for(int i = 0; i < 9; i++){
-         int temp = puzzle[i][swap[0]];
-         puzzle[i][swap[0]] = puzzle[i][swap[1]];;
-         puzzle[i][swap[1]] = temp;
-      }
-      memset(swap,0,sizeof(swap)); 
-   }
+void swap_rowBlocks(int **puzzle){
+   srand(time(NULL));
+   int swap[2] = {0};
 
    // Swap Row Blocks
    while(swap[0] == swap[1]){
@@ -109,8 +139,10 @@ void generateSudoku(int **puzzle){
       }
       rotate_arr(last_arr,1);
    }
-
-   swap(puzzle);
+   
+   swap_rows(puzzle);
+   swap_col(puzzle);
+   //swap_rowBlocks(puzzle);
    
    // Clear numbers to create the puzzle
    /*for(int amount_num = 81; amount_num > 30; amount_num--){
@@ -139,24 +171,6 @@ int **createBasePuzzle(){
   
     
    return puzzle;
-}
-
-void display_screen(int **puzzle){
-
-   char divider[35] = "+---------+---------+---------+";
-
-   printf("%s\n", divider);
-   for (int i = 0; i < 9; i++) {
-
-      for (int j = 0; j < 9; j++) {
-
-         if(j == 0 || j % 3 == 0){printf("|");}
-         printf(" %d ", puzzle[i][j]);
-         if((j+1) == 9){printf("|");}
-      }
-      printf("\n");
-      if((i+1) % 3 == 0){printf("%s\n", divider);}
-   }
 }
 
 int main(void) {
